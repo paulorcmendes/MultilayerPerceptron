42879a17e3c951855008b57719d9c2001b6dc40d
paulorcmendes <mendesdpn@gmail.com>
2017-12-29 11:24:22 -0300

    training and testing sets separated

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -6,4 +6,14 @@ Created on Fri Dec 29 10:35:24 2017
 """
 import numpy as np
 
+my_data = np.genfromtxt('iris.csv', delimiter=';')
+#normalizing values
+for i in range(4):
+    my_data[:,i] = ( my_data[:,i] - min(my_data[:,i]) ) / (max(my_data[:,i]) - min(my_data[:,i]))
+
+train = list(my_data[0:35])+ list(my_data[50:85])+ list(my_data[100:135])
+test  = list(my_data[35:50])+ list(my_data[85:100])+ list(my_data[135:150])
+
+print test.__len__()
+
 
